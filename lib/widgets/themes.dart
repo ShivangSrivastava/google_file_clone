60f8c2c8cbfe7ee228629655388c15cb9b66d858
ShivangSrivastava <shivangsrivastava157@gmail.com>
2023-08-02 00:01:50 +0530

    update font family

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class MyTheme {
   static ThemeData darkTheme(BuildContext context) {
     return ThemeData(
+      fontFamily: GoogleFonts.openSans().fontFamily,
       brightness: Brightness.dark,
       scaffoldBackgroundColor: Vx.hexToColor("#2D2E32"),
       cardColor: Vx.hexToColor("#1F1F1F"),
